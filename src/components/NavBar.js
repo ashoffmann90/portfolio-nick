6b0dae425d8c0cf0235073abfeeac4fb1c1489e9
Andrew Hoffmann <ashoffmann90@gmail.com>
2020-12-13 20:17:53 -0800

    less installed, navbar started

---
+++ src/components/NavBar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function NavBar() {
+    return (
+        <div>
+            <header>
+                <h1>Nick Almanza</h1>
+                <nav>
+                    <a href='#home'>Home</a>
+                    <a href='#videos'>Videos</a>
+                    <a href='#me'>Me</a>
+                    <a href='#contact'>Contact</a>
+                </nav>
+            </header>
+        </div>
+    )
+}
+
+export default NavBar
