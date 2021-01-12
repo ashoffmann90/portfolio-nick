db81dca471176f652219207dd1653b13be171f99
Andrew Hoffmann <ashoffmann90@gmail.com>
2021-01-11 19:31:48 -0800

    fonts applied, working on stacking videos bug

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import NavBar from './components/NavBar'
 import Home from './components/Home'
 import Videos from './components/Videos'
-import Me from './components/Me'
 import Contact from './components/Contact'
 import BehindTheScenes from './components/BehindTheScenes'
 
@@ -19,9 +18,6 @@ function App() {
       </Route>
       <Route exact path='/videos'>
         <Videos/>
-      </Route>
-      <Route exact path='/me'>
-        <Me/>
       </Route>
       <Route exact path='/contact'>
         <Contact/>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,12 +5,14 @@ function NavBar() {
     return (
         <div className='nav-div'>
             <header>
-                <h1>Nick Almanza</h1>
+                <div className='name-title-wrapper'>
+                    <h1>Nick Almanza</h1>
+                    <h2>filmmaker / steadycam operator</h2>
+                </div>
                 <nav>
                     <NavLink to='/'>Home</NavLink>
                     <NavLink to='/videos'>Videos</NavLink>
-                    <NavLink to='/bts'>Behind the Scenes</NavLink>
-                    <NavLink to='/me'>Me</NavLink>
+                    <NavLink to='/bts'>BTS</NavLink>
                     <NavLink to='/contact'>Contact</NavLink>
                 </nav>
             </header>
