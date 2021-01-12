701fb720e5653feec1527d6e54b45bc655ac22e6
Andrew Hoffmann <ashoffmann90@gmail.com>
2021-01-12 14:56:19 -0800

    bug-fix/ fonts not loading in production/added styled-components to iport fonts

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import { Route } from 'react-router';
 import { BrowserRouter as Router } from 'react-router-dom';
+import {createGlobalStyle} from 'styled-components'
+
+import AnotherFlight from './assets/fonts/Another Flight.otf'
+import Basicaline from './assets/fonts/Basicaline-NonCommercial.otf'
 
 import NavBar from './components/NavBar'
 import Home from './components/Home'
@@ -8,9 +12,34 @@ import Videos from './components/Videos'
 import Contact from './components/Contact'
 import BehindTheScenes from './components/BehindTheScenes'
 
+const GlobalStyle = createGlobalStyle`
+@font-face{
+  font-family: 'Another Flight';
+  src: local('Another Flight'), local('AnotherFlight'), url('${AnotherFlight}') format('opentype');
+}
+@font-face{
+  font-family: 'Basicaline';
+  src: local('Basicaline-NonCommercial'), local('Basicaline NonCommercial'), local('Basicaline-Non Commercial'), url('${Basicaline}') format('opentype')
+}
+
+p, li{
+  font-family: @header-font;
+}
+
+h1, h2{
+  font-family: @header-font;
+}
+
+a{
+  font-family: @text-font;
+}
+`
+
+
 function App() {
   return (
     <div>
+      <GlobalStyle/>
       <Router>
       <NavBar/>
       <Route exact path='/'>
