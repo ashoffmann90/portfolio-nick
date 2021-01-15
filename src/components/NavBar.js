396853af24d3077ba2c5f24aac8c1c917759f15a
Andrew Hoffmann <ashoffmann90@gmail.com>
2021-01-14 16:31:20 -0800

    contact updated, bts finished

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom'
 
+import logo from '../assets/nick-bts-images/Name_Title.jpg'
+
 function NavBar() {
     return (
         <div className='nav-div'>
             <header>
                 <div className='name-title-wrapper'>
-                    <h1>Nick Almanza</h1>
-                    <h2>filmmaker / steadycam operator</h2>
+                    <img src={logo} alt='Nick Almanza Filmmaker/Steadycam Operator'/>
                 </div>
+                
                 <nav>
                     <NavLink to='/'>Home</NavLink>
                     <NavLink to='/videos'>Videos</NavLink>
